533e9ce78e7bd04008258da6cfe875660a04b9f7
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-04 17:03:18 +0300

    feat: add back and delete more code

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -1,4 +1,4 @@
-import { SCENES } from '../constants';
+import { SCENES, TEXTURES } from '../constants';
 import { Scene } from 'phaser';
 
 export default class Main extends Scene {
@@ -7,7 +7,14 @@ export default class Main extends Scene {
   }
 
   create() {
+    this.addBackground();
+  }
 
+  addBackground() {
+    const background = this.add.sprite(-50, -50, TEXTURES.BACKGROUND);
+    background.setOrigin(0, 0);
+    background.displayWidth = this.game.config.width + 100;
+    background.displayHeight = this.game.config.height + 100;
   }
 
   update(time, delta) {
